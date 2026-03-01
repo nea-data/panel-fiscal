ff035f0acd5420cf23db33f3a0d7a6774b1beca7
nea-data <neadata.contacto@gmail.com>
2026-03-01 15:09:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -719,11 +719,12 @@ elif seccion == "🛠 Administración":
 
     from auth.guard import require_admin
     from auth.users import (
-        set_user_status,
-        set_user_role,
-        get_user_by_email,
-        set_user_password,
-    )
+    authenticate_user, 
+    set_user_status, 
+    set_user_role, 
+    get_user_by_email, 
+    set_user_password
+)
     from auth.subscriptions import (
         create_subscription,
         renew_subscription,
